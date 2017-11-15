156328236e61820776d769bc63a2190f3b01e577
Andrii Shabunko <asha@softserveinc.com>
2017-11-15 11:34:03 +0200

    Initial commit

---
+++ TestProject/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  TestProject
+//
+//  Created by Andrii Shabunko on 11/15/17.
+//  Copyright © 2017 Andrii Shabunko. All rights reserved.
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear
+    }
+    
+    // MARK: - Methods
+}
